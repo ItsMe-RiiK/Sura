62bde50e507e1ec3a987fa07aeccd6c0845a762a
ItsMe-RiiK <syakirr266@gmail.com>
2026-08-13 14:24:43 +0700

    implement dynamic system theme support

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,36 @@
 
 #include <QApplication>
 #include <QIcon>
+#include <QPalette>
 #include <QStringList>
+#include <QStyleFactory>
+#include <QStyleHints>
+
+void applyTheme(QApplication& app)
+{
+  app.setStyle(QStyleFactory::create("Fusion"));
+
+  if (QGuiApplication::styleHints()->colorScheme() == Qt::ColorScheme::Dark) {
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(35, 35, 35));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    app.setPalette(darkPalette);
+  }
+  else {
+    app.setPalette(app.style()->standardPalette());
+  }
+}
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +39,14 @@ int main(int argc, char* argv[])
   app.setApplicationName("sura");
   app.setDesktopFileName("sura");
   app.setWindowIcon(QIcon(":/images/icon.png"));
+
+  applyTheme(app);
+
+  // Auto-update theme if user changes system setting while app is running
+  QObject::connect(
+    QGuiApplication::styleHints(), &QStyleHints::colorSchemeChanged, &app,
+    [&app](Qt::ColorScheme) { applyTheme(app); }
+  );
 
   Sura viewer;
 
